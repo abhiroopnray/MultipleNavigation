caeeed8577e71328e4d363add68720450605f649
Abhiroop Nandi Ray <abhiroopnandi.ray@sonimtech.com>
2021-04-14 04:45:23 +0530

    HomeFragment ui hardcoded implementation

--- app/src/main/java/com/abhiroop/multiplenavigation/fragment/home/HomeFragment.java
+++ app/src/main/java/com/abhiroop/multiplenavigation/fragment/home/HomeFragment.java
@@ -8,6 +8,8 @@ import android.os.Bundle;
 import androidx.annotation.NonNull;
 import androidx.annotation.Nullable;
 import androidx.fragment.app.Fragment;
+import androidx.recyclerview.widget.LinearLayoutManager;
+import androidx.recyclerview.widget.RecyclerView;
 
 import android.view.LayoutInflater;
 import android.view.View;
@@ -19,26 +21,37 @@ import com.abhiroop.multiplenavigation.R;
 public class HomeFragment extends Fragment {
 
     private HomeViewModel mViewModel;
+    private RecyclerView recyclerView;
 
     public static HomeFragment newInstance() {
         return new HomeFragment();
     }
 
-    private HomeFragment homeFragment;
 
     public View onCreateView(@NonNull LayoutInflater inflater,
                              ViewGroup container, Bundle savedInstanceState) {
         mViewModel =
                 new ViewModelProvider(this).get(HomeViewModel.class);
         View root = inflater.inflate(R.layout.home_fragment, container, false);
-        final TextView textView = root.findViewById(R.id.tv);
+        /*final TextView textView = root.findViewById(R.id.tv);
         mViewModel.getText().observe(getViewLifecycleOwner(), new Observer<String>() {
             @Override
             public void onChanged(@Nullable String s) {
                 textView.setText(s);
             }
-        });
+        });*/
+        initView(root);
         return root;
+    }
+
+    private void initView(View view){
+        recyclerView = view.findViewById(R.id.home_recycler_view);
+        LinearLayoutManager linearLayoutManager = new LinearLayoutManager(getContext());
+        recyclerView.setLayoutManager(linearLayoutManager);
+        recyclerView.hasFixedSize();
+        HomeFragmentAdapter adapter = new HomeFragmentAdapter();
+        recyclerView.setAdapter(adapter);
+
     }
 
     @Override
---
+++ app/src/main/java/com/abhiroop/multiplenavigation/fragment/home/HomeFragmentAdapter.java
@@ -0,0 +1,43 @@
+package com.abhiroop.multiplenavigation.fragment.home;
+
+import android.content.Context;
+import android.util.AttributeSet;
+import android.util.SparseArray;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
+import androidx.recyclerview.widget.RecyclerView;
+
+import com.abhiroop.multiplenavigation.R;
+
+public class HomeFragmentAdapter extends RecyclerView.Adapter<RecyclerView.ViewHolder> {
+
+
+    @NonNull
+    @Override
+    public RecyclerView.ViewHolder onCreateViewHolder(@NonNull ViewGroup parent, int viewType) {
+        View view = LayoutInflater.from(parent.getContext()).inflate(R.layout.item_card, parent, false);
+        Holder holder = new Holder(view);
+        return holder;
+    }
+
+    @Override
+    public void onBindViewHolder(@NonNull RecyclerView.ViewHolder holder, int position) {
+
+    }
+
+    @Override
+    public int getItemCount() {
+        return 10;
+    }
+
+    public static class Holder extends RecyclerView.ViewHolder{
+
+        public Holder(@NonNull View itemView) {
+            super(itemView);
+        }
+    }
+}
